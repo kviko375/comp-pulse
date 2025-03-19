d0a48e521fd8a4f829f20e02c9d2d856cc838c31
kviko375 <dzieciuki@gmail.com>
2025-03-19 15:33:45 +0000

    Add files via upload

--- src/pages/ReportViewer.tsx
+++ src/pages/ReportViewer.tsx
@@ -539,26 +539,12 @@ function ReportViewer() {
         </div>
         ` : `
         <div class="container">
-          <div class="header">
-            <a href="${window.location.origin}" class="logo">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M3 3v18h18"></path>
-                <path d="M13 17V9"></path>
-                <path d="M18 17V5"></path>
-                <path d="M8 17v-3"></path>
-              </svg>
-              <span>CompetitivePulse</span>
-            </a>
-          </div>
           <div class="report-content">
         `}
         
         ${bodyContent}
         
         ${forPdf ? '' : `
-          </div>
-          <div class="footer">
-            <p>© ${new Date().getFullYear()} CompetitivePulse. All rights reserved. <a href="${window.location.origin}">Visit CompetitivePulse!2</a></p>
           </div>
         </div>
         `}
